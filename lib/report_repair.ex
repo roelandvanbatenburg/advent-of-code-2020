eb395928830610dceac42cc0689e43248f0f34a6
Roeland van Batenburg <roeland@enrise.com>
2020-12-04 20:04:26 +0100

    01 part 2

--- lib/report_repair.ex
+++ lib/report_repair.ex
@@ -1,16 +1,30 @@
 defmodule ReportRepair do
   @moduledoc """
-  Find the two numbers in the list that add to 2020 and return the product
+  Find the two or three numbers in the list that add to 2020 and return the product
   """
 
-  @spec run(list(integer)) :: integer
-  def run(list) do
+  @spec run2(list(integer)) :: integer
+  def run2(list) do
     list
-    |> Enum.find_value(fn element -> find_2020(element, list) end)
+    |> Enum.find_value(fn element -> find_2020(element, element, list) end)
   end
 
-  defp find_2020(element, list) do
-    case find_match_2020(element, list) do
+  @spec run3(list(integer)) :: integer
+  def run3(list) do
+    list
+    |> Enum.find_value(fn element -> find_2020_in_2(element, list) end)
+  end
+
+  defp find_2020_in_2(first_element, list) do
+    case list
+         |> Enum.find_value(fn element -> find_2020(first_element + element, element, list) end) do
+      nil -> nil
+      product -> first_element * product
+    end
+  end
+
+  defp find_2020(sum_so_far, element, list) do
+    case find_match_2020(sum_so_far, list) do
       nil -> nil
       second_element -> element * second_element
     end
--- test/report_repair_test.exs
+++ test/report_repair_test.exs
@@ -1,11 +1,19 @@
 defmodule ReportRepairTest do
   use ExUnit.Case
 
-  test "finds the product" do
-    assert 4036 == ReportRepair.run([2, 0, 5, 2018, 7, 9])
+  describe "in two" do
+    test "finds the product" do
+      assert 4036 == ReportRepair.run2([2, 0, 5, 2018, 7, 9])
+    end
+
+    test "nil when not possible" do
+      assert nil == ReportRepair.run2([2, 0, 5, 2017, 7, 9])
+    end
   end
 
-  test "nil when not possible" do
-    assert nil == ReportRepair.run([2, 0, 5, 2017, 7, 9])
+  describe "in three" do
+    test "finds the product" do
+      assert 241_861_950 == ReportRepair.run3([1721, 979, 366, 299, 675, 1456])
+    end
   end
 end
