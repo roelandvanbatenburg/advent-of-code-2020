a39db2946359ad1be43af3cc4b26666ae2147c30
Roeland van Batenburg <roeland@enrise.com>
2020-12-05 20:30:35 +0100

    simplify implementation

--- lib/binary_boarding.ex
+++ lib/binary_boarding.ex
@@ -4,28 +4,10 @@ defmodule BinaryBoarding do
   """
 
   @spec seat_id(<<_::80>>) :: number
-
-  def seat_id(input), do: input |> parse() |> id
-
-  defp parse(<<row::binary-size(7), column::binary-size(3)>>) do
-    {parse_row(row), parse_column(column)}
+  def seat_id(input) do
+    input
+    |> String.replace(~r/[FL]/, "0")
+    |> String.replace(~r/[BR]/, "1")
+    |> String.to_integer(2)
   end
-
-  defp id({column, row}), do: column * 8 + row
-
-  defp parse_row(<<"F", rest::binary>>), do: 0 + parse_row(rest)
-  defp parse_row(<<"B", rest::binary-size(6)>>), do: 64 + parse_row(rest)
-  defp parse_row(<<"B", rest::binary-size(5)>>), do: 32 + parse_row(rest)
-  defp parse_row(<<"B", rest::binary-size(4)>>), do: 16 + parse_row(rest)
-  defp parse_row(<<"B", rest::binary-size(3)>>), do: 8 + parse_row(rest)
-  defp parse_row(<<"B", rest::binary-size(2)>>), do: 4 + parse_row(rest)
-  defp parse_row(<<"B", rest::binary-size(1)>>), do: 2 + parse_row(rest)
-  defp parse_row("B"), do: 1
-  defp parse_row(""), do: 0
-
-  defp parse_column(<<"L", rest::binary>>), do: 0 + parse_column(rest)
-  defp parse_column(<<"R", rest::binary-size(2)>>), do: 4 + parse_column(rest)
-  defp parse_column(<<"R", rest::binary-size(1)>>), do: 2 + parse_column(rest)
-  defp parse_column("R"), do: 1
-  defp parse_column(""), do: 0
 end
