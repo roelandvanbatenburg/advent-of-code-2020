37fd4352accdc7a22e386205dab01eb3fbcd2b41
Roeland van Batenburg <roeland@enrise.com>
2020-12-21 22:28:59 +0100

    WIP

--- lib/jurassic_jigsaw.ex
+++ lib/jurassic_jigsaw.ex
@@ -24,7 +24,7 @@ defmodule JurassicJigsaw do
 
   defmodule Tile do
     @moduledoc "tile"
-    @enforce_keys [:corners, :id]
+    @enforce_keys [:corners, :id, :tile]
     defstruct @enforce_keys
 
     # (# or .)
@@ -32,6 +32,7 @@ defmodule JurassicJigsaw do
     @type line :: list()
     @type t() :: %__MODULE__{
             corners: list(list()),
+            tile: list(list()),
             id: integer
           }
 
@@ -39,8 +40,25 @@ defmodule JurassicJigsaw do
     def create(id_line, tile) do
       %__MODULE__{
         id: String.to_integer(String.slice(id_line, 5..8)),
+        tile: tile,
         corners: get_corners(tile)
       }
+    end
+
+    @spec is_corner?(Tile.t(), list(Tile.t())) :: boolean
+    def is_corner?(tile, tiles) do
+      tiles
+      |> Enum.reduce(0, fn target, neighbour_cnt ->
+        if target.id == tile.id do
+          neighbour_cnt
+        else
+          if Enum.any?(target.corners, fn corner -> Enum.member?(tile.corners, corner) end) do
+            neighbour_cnt + 1
+          else
+            neighbour_cnt
+          end
+        end
+      end) === 2
     end
 
     defp get_corners(tile) do
@@ -80,22 +98,7 @@ defmodule JurassicJigsaw do
     @spec find_corners(list(Tile.t())) :: list(Tile.t())
     def find_corners(tiles) do
       tiles
-      |> Enum.filter(&is_corner?(&1, tiles))
-    end
-
-    defp is_corner?(tile, tiles) do
-      tiles
-      |> Enum.reduce(0, fn target, neighbour_cnt ->
-        if target.id == tile.id do
-          neighbour_cnt
-        else
-          if Enum.any?(target.corners, fn corner -> Enum.member?(tile.corners, corner) end) do
-            neighbour_cnt + 1
-          else
-            neighbour_cnt
-          end
-        end
-      end) === 2
+      |> Enum.filter(&Tile.is_corner?(&1, tiles))
     end
 
     @spec id_product(list(Tile.t())) :: integer
@@ -109,5 +112,51 @@ defmodule JurassicJigsaw do
     @moduledoc """
     Second puzzle
     """
+
+    @spec solve(list(Tile.t())) :: map()
+    def solve(tiles) do
+      corner_piece = Enum.find(tiles, &Tile.is_corner?(&1, tiles))
+      tiles = List.delete(tiles, corner_piece)
+      positions = Map.put(%{}, {0, 0}, corner_piece)
+
+      solve(tiles, positions)
+    end
+
+    defp solve([], positions), do: positions
+
+    defp solve(tiles, positions) do
+      {location, placed_tile, original_tile} =
+        Enum.find(tiles, fn tile ->
+          case match_orientation(tile, positions) do
+            nil -> nil
+          end
+        end)
+
+      tiles = List.delete(tiles, original_tile)
+      positions = Map.put(positions, location, placed_tile)
+
+      solve(tiles, positions)
+    end
+
+    defp match_orientation(tile, positions) do
+      possible_positions(positions)
+      |> Enum.find_value(fn position -> place_tile(position, tile, positions) end)
+    end
+
+    defp place_tile(position, tile, _positions) do
+      # todo: return {location, rotated/flipped tile, orginal tile} if it matched
+      # nil if it doesn't fit here
+      {position, tile, tile}
+    end
+
+    defp possible_positions(positions) do
+      filled = Map.keys(positions)
+
+      # todo: +/- x/y for each
+      filled
+      |> Enum.flat_map(fn position -> [position] end)
+      |> Enum.uniq()
+      |> Enum.filter(fn position -> Enum.member?(filled, position) end)
+    end
   end
 end
